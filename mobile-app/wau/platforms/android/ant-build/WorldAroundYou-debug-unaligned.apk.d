ecd424f4cfc130e0efcf2d4744408b6c7fb594e9
Robert Stein <roddirockz@googlemail.com>
2014-07-09 17:58:41 +0200

    homepage templates, js and css

--- mobile-app/wau/platforms/android/ant-build/WorldAroundYou-debug-unaligned.apk.d
+++ mobile-app/wau/platforms/android/ant-build/WorldAroundYou-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/WorldAroundYou-debug-unaligned.apk : \
-/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/WorldAroundYou.ap_ \
-/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes.dex \
-/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/roddirockz/wau/mobile-app/wau/platforms/android/ant-build/WorldAroundYou-debug-unaligned.apk : \
+/Users/roddirockz/wau/mobile-app/wau/platforms/android/ant-build/WorldAroundYou.ap_ \
+/Users/roddirockz/wau/mobile-app/wau/platforms/android/ant-build/classes.dex \
+/Users/roddirockz/wau/mobile-app/wau/platforms/android/CordovaLib/ant-build/classes.jar \
