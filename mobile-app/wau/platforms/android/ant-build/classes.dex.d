18e6a63c36d04e2b73b9d91b0f2c532a827e3e07
Robert Stein <roddirockz@googlemail.com>
2014-07-09 14:35:24 +0200

    added mobile app with camera functionality

---
+++ mobile-app/wau/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes.dex : \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/BuildConfig.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/R$attr.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/R$drawable.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/R$string.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/R$xml.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/R.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/com/wau/app/WorldAroundYou.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/org/apache/cordova/camera/CameraLauncher.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/org/apache/cordova/camera/ExifHelper.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/classes/org/apache/cordova/camera/FileHelper.class \
+/Users/roddirockz/wau-projectz/wau/mobile-app/wau/platforms/android/ant-build/dexedLibs/classes-3dd8d2e213f54c429f4cda46e7aaf4df.jar \
