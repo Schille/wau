7c3c74b528359a1c1db0aceaf409f61d47decc7b
Benedict Holste <benedict@bholste.net>
2014-07-09 16:42:33 +0200

    added templates and routes for wau

--- wau/wau.py
+++ wau/wau.py
@@ -3,10 +3,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def hello(name):
-	return 'Hello World!' + name
+# Show index 
+@app.route('/')
+def show_index():
+	return 'index'
 
+# Show single image
+@app.route('/image/<uuid>')
+def show_image(uuid=None):
+	return 'image ' + uuid
+
+# Run the app
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
